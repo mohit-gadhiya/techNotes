f44f543848628cebb4fea5e354b54c4c7d476f94
mohit-gadhiya <gadhiyamohit208@gmail.com>
2022-08-28 00:02:54 +0530

    controllers done

--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const router = express.Router();
+
 const path = require('path');
 
-const router = express.Router();
 
 router.get('^/$|index(.html)?',(req,res)=>{
     res.sendFile(path.join(__dirname,'..','views','index.html'));
